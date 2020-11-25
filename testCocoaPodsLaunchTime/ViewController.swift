68cbc333168db45dbc14662764300389f74abe45
James Tang <james@goodnotesapp.com>
2020-11-25 10:05:15 +0800

    Initial Commit

---
+++ testCocoaPodsLaunchTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testCocoaPodsLaunchTime
+//
+//  Created by James Tang on 25/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
